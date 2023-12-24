5648a190fce40963f22526411d3afa5c06044114
hamzaali15 <malikhamzaali48@gmail.com>
2023-12-25 00:14:23 +0500

    changes reversed

--- v1/functions.php
+++ v1/functions.php
@@ -4,8 +4,6 @@ namespace Leekim;
 
 use TCPDF;
 use TCPDF_FONTS;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
 
 
 class PDF020 extends TCPDF
@@ -402,19 +400,8 @@ function constitutionLimitedByGuaranteePdf($html_template, $pdf_name)
     $tcpdf->writeHTML($html_template, true, 0, true, true);
     $tcpdf->Ln();
 
-    // Add markup.
-    $tcpdf->addPage();
-    // Load Twig
-    $loader = new FilesystemLoader(__DIR__ . '/../templates/printouts/constitution'); // Adjust the path to your templates directory
-    $twig = new Environment($loader);
-    // Render the first Twig template to generate HTML content
-    $partialEnd = $twig->load('constitution-limited-by-guarantee.html.twig');
-    $markup = $partialEnd->render();
-    $tcpdf->setCellHeightRatio(1.7);
-    // 본문 글자 크기.
-    $tcpdf->SetFont('freeserif', '', 11);
-    $tcpdf->writeHTML($markup, TRUE, 0, TRUE, TRUE);
-    $tcpdf->Ln();
+    // Additional markup here if needed
+
     $tcpdf->lastPage();
 
     // $company_name = str_replace(' ', '_', $company->getTitle());
@@ -3747,7 +3734,7 @@ function registerOfMembers($html, $pdf_name, $output_type = "I")
     return $tcpdf->Output($pdf_name, $output_type);
 }
 
-function serviceIndemnityAgreementSFA($html_content, $pdf_name, $templateName, $output_type = 'I')
+function serviceIndemnityAgreementSFA($html_content, $pdf_name, $output_type = 'I')
 {
 
 
@@ -3758,18 +3745,7 @@ function serviceIndemnityAgreementSFA($html_content, $pdf_name, $templateName, $
     // ];
     // $html = $this->renderer->render($html_template);
     $id = '1st';
-    if (in_array($templateName,
-        ['tcpdf-05-service-fee-agreement.html.twig',
-        'tcpdf-05-service-fee-agreement-principal-03.html.twig',
-        'tcpdf-05-service-fee-agreement-principal.html.twig',
-        'tcpdf-05-service-fee-agreement-principal-04.html.twig'])
-    ) {
-        // For 2nd, 2nd/BlkChn, 2nd-Principal and 2nd-Principal/BlkChn documents
-        $title_name = '법인 유지 비용 계약서';
-    } else {
-        $title_name = '법인 설립 및 유지 비용 계약서';
-    }
-
+    $title_name = '법인 설립 및 유지 비용 계약서';
 
     $tcpdf = new PDFSFA(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, TRUE, 'UTF-8', FALSE);
 
@@ -3780,7 +3756,7 @@ function serviceIndemnityAgreementSFA($html_content, $pdf_name, $templateName, $
      </style>
      <table border="0" cellspacing="0" cellpadding="0">
        <tr>
-         <td align="left"  style="width:50%;"><img src="../public/assets/printouts/leekim-service.png"></td>
+         <td align="left"  style="width:50%;"><img src="../public/assets/printouts/korean_logo.png"></td>
          <td align="right" style="width:50%;"><br><br>
            <strong>LEE KIM ALLIANCE PTE. LTD.</strong><br>
            <span>111 Somerset, #06-07L</span><br>
@@ -3954,13 +3930,13 @@ function serviceIndemnityAgreementSFA($html_content, $pdf_name, $templateName, $
 //    */
 function appointmentOfAuditors($html, $pdf_name, $output_type = "I")
 {
-    $datas = DocumentWrapper::appointmentOfAuditors($company);
-    $html_template = [
-      '#theme' => 'tcpdf_46_01_appointment_of_auditors',
-      '#datas' => $datas,
-    ];
-    $html = $this->renderer->render($html_template);
-    ob_start();
+    // $datas = DocumentWrapper::appointmentOfAuditors($company);
+    // $html_template = [
+    //   '#theme' => 'tcpdf_46_01_appointment_of_auditors',
+    //   '#datas' => $datas,
+    // ];
+    // $html = $this->renderer->render($html_template);
+    // ob_start();
 
 
     $helvetica = TCPDF_FONTS::addTTFfont(getcwd() . '/../Helvetica.ttf', 'TrueTypeUnicode', '', 32);
@@ -3968,30 +3944,28 @@ function appointmentOfAuditors($html, $pdf_name, $output_type = "I")
 
     $tcpdf = new PDFSFA(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, TRUE, 'UTF-8', FALSE);
 
-    // just for testing
-    $companyTitleStr = $datas['company_name'] . ' ' . $datas['field_company_type'];
-    $addressStr = $datas['address'] ? '<span>' . $datas['address'] . '</span><br>' : '';
-    $address2Str = $datas['address_2'] ? '<span>' . $datas['address_2'] . '</span><br>' : '';
-    $contactNoStr = $datas['contact_no'] ? '<span>TEL: ' . $datas['contact_no'] . '</span><br>' : '';
-    $mailStr = $datas['mail'] ? '<span>Email: ' . $datas['mail'] . '</span><br>' : '';
-    $tcpdf->setCustomHeaderText('
-        <style>
-            span {font-size: 11px;font-weight: normal;font-family: Arial, Helvetica, sans-serif;}
-        </style>
-        <table border="0" cellspacing="0" cellpadding="0">
-            <tr>
-                <td align="left"  style="width:50%;"></td>
-                <td align="right" style="width:50%;"><br><br>
-                    <strong>' . $companyTitleStr . '</strong><br>
-                    ' . $addressStr . '
-                    ' . $address2Str . '
-                    ' . $contactNoStr . '
-                    ' . $mailStr . '
-                </td>
-            </tr>
-        </table>
-    ');
-    //till here
+    // $companyTitleStr = $datas['company_name'] . ' ' . $datas['field_company_type'];
+    // $addressStr = $datas['address'] ? '<span>' . $datas['address'] . '</span><br>' : '';
+    // $address2Str = $datas['address_2'] ? '<span>' . $datas['address_2'] . '</span><br>' : '';
+    // $contactNoStr = $datas['contact_no'] ? '<span>TEL: ' . $datas['contact_no'] . '</span><br>' : '';
+    // $mailStr = $datas['mail'] ? '<span>Email: ' . $datas['mail'] . '</span><br>' : '';
+    // $tcpdf->setCustomHeaderText('
+    //   <style>
+    //     span {font-size: 11px;font-weight: normal;font-family: Arial, Helvetica, sans-serif;}
+    //   </style>
+    //   <table border="0" cellspacing="0" cellpadding="0">
+    //     <tr>
+    //       <td align="left"  style="width:50%;"></td>
+    //       <td align="right" style="width:50%;"><br><br>
+    //         <strong>' . $companyTitleStr . '</strong><br>
+    //         ' . $addressStr . '
+    //         ' . $address2Str . '
+    //         ' . $contactNoStr . '
+    //         ' . $mailStr . '
+    //       </td>
+    //     </tr>
+    //   </table>
+    // ');
 
     $tcpdf->SetHeaderMargin(PDF_MARGIN_FOOTER);
     $tcpdf->setPrintFooter(FALSE);
@@ -4003,16 +3977,12 @@ function appointmentOfAuditors($html, $pdf_name, $output_type = "I")
 
     $tcpdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
     $tcpdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-    // just for testing
-    $author = \Drupal::currentUser()->getAccountName();
-    $tcpdf->SetCreator($author);
-    $tcpdf->SetAuthor($author);
-    $tcpdf->SetTitle("APPOINTMENT OF AUDITORS ($companyTitleStr)");
-    $tcpdf->SetSubject("APPOINTMENT OF AUDITORS ($companyTitleStr)");
-    $tcpdf->SetKeywords("APPOINTMENT OF AUDITORS ($companyTitleStr)");
-    // till here
-
+    // $author = \Drupal::currentUser()->getAccountName();
+    // $tcpdf->SetCreator($author);
+    // $tcpdf->SetAuthor($author);
+    // $tcpdf->SetTitle("APPOINTMENT OF AUDITORS ($companyTitleStr)");
+    // $tcpdf->SetSubject("APPOINTMENT OF AUDITORS ($companyTitleStr)");
+    // $tcpdf->SetKeywords("APPOINTMENT OF AUDITORS ($companyTitleStr)");
     $tcpdf->addPage();
     $tcpdf->SetFont($helvetica, '', 11);
     $tcpdf->writeHTML($html, TRUE, 0, TRUE, TRUE, '');
